46aa836e7bacbbab5810261b704e7165f2b28e78
MP <markpaololibunao@gmail.com>
2022-10-02 07:11:30 +0800

    Updates blankline configuration

--- lua/indent-blackline.lua
+++ lua/indent-blackline.lua
@@ -1,17 +1,18 @@
-vim.opt.list = true
-vim.opt.listchars:append "eol:↴"
-vim.opt.listchars:append "space:⋅"
+--vim.opt.list = true
+--vim.opt.listchars:append "eol:↴"
+--vim.opt.listchars:append "space:⋅"
+--vim.opt.listchars:append "tab:⋅⋅"
 
 require("indent_blankline").setup {
-    show_trailing_blankline_ident = false,
-    show_end_of_line = false,
-    space_char_blankline = " ",
-    buftype_exclude = { "terminal", "nofile" },
-    filetype_exclude = {
-        "help",
-        "terminal",
-        "startify",
-        "lspinfo",
-    },
-    show_first_indent_level = true,
+  show_trailing_blankline_ident = false,
+  show_end_of_line = false,
+  space_char_blankline = " ",
+  buftype_exclude = { "terminal", "nofile" },
+  filetype_exclude = {
+      "help",
+      "terminal",
+      "startify",
+      "lspinfo",
+  },
+  show_first_indent_level = false,
 }
